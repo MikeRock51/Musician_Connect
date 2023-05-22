10e998b12d86344a6bb59ce25746d57d19fbfb4b
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-22 05:08:23 +0100

    Complete user model

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 
 from models import storage
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -14,6 +14,12 @@ class User(BaseModel, Base):
     userType = Column(String(20), nullable=False)
     firstName = Column(String(128), nullable=False)
     lastName = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    reviews = relationship('Review', backref='user', cascade='all, delete')
     if userType == "Musician":
         alias = Column(String(128), nullable=True)
-        instruments = 
+        instruments = relationship('Instrument', backref='players', cascade='all, delete')
+        price_by_hour = Column(Integer, nullable=False, default=0)
