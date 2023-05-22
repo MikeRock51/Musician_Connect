3c18c67e882effa7173995925abf9f5d5f54a754
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-22 17:27:57 +0100

    Add api app

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Flask Application"""
+
+from flask import Flask, make_response, jsonify
+from models import storage
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.teardown_appcontext
+def tearDown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def notFound(err):
+    """Returns a json formatted 404 error"""
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=7000, threaded=True)
