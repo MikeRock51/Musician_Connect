3800332af31d50b437a5f0225cfc454e22c0b265
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-23 11:38:46 +0100

    Add add more /users route

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify, make_response
 
 
-@app_views.route('/users')
+@app_views.route('/users', strict_slashes=False)
 def allUsers():
     """Returns a JSON formatted list of all users in database"""
     usersList = []
@@ -17,3 +17,20 @@ def allUsers():
         usersList.append(user.toDict())
 
     return jsonify(usersList)
+
+@app_views.route('/users/type/<userType>', strict_slashes=False)
+def getUserByType(userType):
+    """Returns a JSON formatted list of all users of the requested type"""
+    usersList = []
+    allUsers = storage.all(User)
+
+    for user in allUsers.values():
+        if user.toDict()['userType'].lower() == userType.lower():
+            usersList.append(user.toDict())
+
+    return jsonify(usersList)
+
+@app_views.route('/users/<userId>', strict_slashes=False)
+def getUserById(userId):
+    """Retrieves a user from storage by ID"""
+    return jsonify(storage.get(User, userId).toDict())
