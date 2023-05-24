461afd5eb1902bfdd4209b84d2f6f6cd7a418ee5
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-24 13:20:41 +0100

    Implement POST/PUT methods for /users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,7 +4,7 @@
 from models import storage
 from models.user import User
 from api.v1.views import app_views
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request, abort
 
 
 @app_views.route('/users', strict_slashes=False)
@@ -34,3 +34,50 @@ def getUserByType(userType):
 def getUserById(userId):
     """Retrieves a user from storage by ID"""
     return jsonify(storage.get(User, userId).toDict())
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def createUser():
+    """Creates a new User"""
+    userData = request.get_json()
+    if not userData:
+        abort(400, description="Not a JSON")
+
+    if 'firstName' not in userData:
+        return make_response(jsonify({"error": "Missing First Name"}), 400)
+    if 'lastName' not in userData:
+        return make_response(jsonify({"error": "Missing Last Name"}), 400)
+    if 'email' not in userData:
+        return make_response(jsonify({"error": "Missing Email"}), 400)
+    if 'password' not in userData:
+        return make_response(jsonify({"error": "Missing Password"}), 400)
+    if 'city_id' not in userData:
+        return make_response(jsonify({"error": "Missing City Id"}), 400)
+    if 'userType' not in userData:
+        return make_response(jsonify({"error": "Missing userType"}), 400)
+
+    newUser = User(**userData)
+    newUser.save()
+
+    return make_response(jsonify(newUser.toDict()), 201)
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def updateUser(user_id):
+    """Updates the specified user"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    userData = request.get_json()
+
+    if not userData:
+        abort(400, description="Not a JSON")
+
+    ignoredKeys = ['createdAt', 'updatedAt', 'id', 'userType']
+
+    for key, value in userData.items():
+        if key not in ignoredKeys:
+            setattr(user, key, value)
+
+    user.save()
+
+    return make_response(jsonify(user.toDict()), 200)
