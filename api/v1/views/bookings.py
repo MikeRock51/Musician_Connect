ce5723825f3e4ef8ff88cbaf971cdcdf683fcf6a
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-25 09:37:24 +0100

    Implement GET route for /users/bookings

--- api/v1/views/bookings.py
+++ api/v1/views/bookings.py
@@ -3,12 +3,33 @@
 
 from models import storage
 from models.booking import Booking
-from models,user import User
+from models.user import User
 from api.v1.views import app_views
 from flask import make_response, jsonify, abort, request
 
 
-@app_views.route('/users/<user_id>/bookings', strict_slashes=False)
-def fetchUserBookings(user_id):
-    """Retrieves all the bookings a user was involved in"""
+@app_views.route('/users/<user_type>/<user_id>/bookings', strict_slashes=False)
+def fetchUserBookings(user_id, user_type):
+    """
+        Retrieves all the bookings a user was involved in
+        based of user id and user type
+    """
+    if user_type.lower() not in ['musician', 'client']:
+        return make_response(jsonify({"error": "Invalid User type"}), 400)
 
+    user = storage.get(User, user_id)
+
+    if not user:
+        abort(404)
+
+    userReviews = []
+
+    if user_type == 'musician':
+        reviews = user.musicianBookings
+    else: 
+        reviews = user.clientBookings
+
+    for review in reviews:
+        userReviews.append(Booking(review).toDict())
+
+    return make_response(jsonify(userReviews), 200)
