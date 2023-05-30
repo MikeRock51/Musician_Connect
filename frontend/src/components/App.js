c23ffab53b42eced39106f7c760170d6f9036bb4
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-30 15:19:00 +0100

    Add dynamic carousel

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,17 +2,37 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import Navbar from "./Navbar";
 import Slideshow from './Slideshow';
+import { useState } from 'react';
 
 function App() {
+  const [slideData, setSlideData] = useState([{
+    "imageUrl": "https://scontent.fabv2-1.fna.fbcdn.net/v/t1.6435-9/127536440_680998232855827_8401588747317748180_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG7uHj-jxQnxeprZYwHZoKp5FiR_Ra_NpjkWJH9Fr82mLQxOhcV1FZyNErOGI8c5yK8aLTlx7S3tN6hruuEM7I4&_nc_ohc=-cntwUr1u6cAX-ROQ-Z&_nc_oc=AQkTqGTR3v_O5lim_c3LK2zH3t06Q7bkHr8_f2OifQ3PqKw0-z4M7jozYfMww7Z5IkQ&_nc_ht=scontent.fabv2-1.fna&oh=00_AfASiNiWyjwb4jpGRz8_LTnica17dRIlQAkwaylFKaXK8Q&oe=649D5465",
+    "name": "Michael Adebayo",
+    "instruments": ["Guitar", "Piano", "Voice"],
+    "city": "Dutse-Alhaji"
+  }, {
+    "imageUrl": "https://avatars.githubusercontent.com/u/111021729?v=4",
+    "name": "Michael Olasunkanmi",
+    "instruments": ["Piano", "Violin", "Drums"],
+    "city": "Gwarinpa"
+  }, {
+    "imageUrl": "https://lh3.googleusercontent.com/ogw/AOLn63EIdidZfdc_I9RNUE7EpINxl-OgPw16WRJIx2RB=s32-c-mo",
+    "name": "Mike Rock",
+    "instruments": ["Cello", "Tuba", "Producer"],
+    "city": "Katampe Extension"
+  }])
+
   return (
     <div className="App">
       <Navbar />
       <div className='my-5 text-center'>
-        <h1 className='display-4'>Welcome to Musician Connect!</h1>
-        <h4>The best musicians of Abuja at your fingertips</h4>
+        <h1 className='display-4 teal'>Welcome to Musician Connect!</h1>
+        <h4 className='cinnabar'>The best musicians of Abuja at your fingertips</h4>
       </div>
       <div className="carousel-container">
-        <Slideshow />
+        <Slideshow 
+        data={slideData}
+        />
       </div>
     </div>
   );
--- frontend/src/components/Slideshow.jsx
+++ frontend/src/components/Slideshow.jsx
@@ -1,45 +1,32 @@
 import Carousel from 'react-bootstrap/Carousel';
 
-function Slideshow() {
+function Slideshow(props) {
+  const musicians = props.data;
+
   return (
     <Carousel variant="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://avatars.githubusercontent.com/u/111021729?v=4"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://lh3.googleusercontent.com/ogw/AOLn63EIdidZfdc_I9RNUE7EpINxl-OgPw16WRJIx2RB=s32-c-mo"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://scontent.fabv2-1.fna.fbcdn.net/v/t1.6435-9/127536440_680998232855827_8401588747317748180_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG7uHj-jxQnxeprZYwHZoKp5FiR_Ra_NpjkWJH9Fr82mLQxOhcV1FZyNErOGI8c5yK8aLTlx7S3tN6hruuEM7I4&_nc_ohc=-cntwUr1u6cAX-ROQ-Z&_nc_oc=AQkTqGTR3v_O5lim_c3LK2zH3t06Q7bkHr8_f2OifQ3PqKw0-z4M7jozYfMww7Z5IkQ&_nc_ht=scontent.fabv2-1.fna&oh=00_AfASiNiWyjwb4jpGRz8_LTnica17dRIlQAkwaylFKaXK8Q&oe=649D5465"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {musicians.map((musician, index) => {
+        return (
+        <Carousel.Item key={index}>
+          <img
+            className="d-block w-100"
+            src={musician.imageUrl}
+            alt="Musician"
+          />
+          <Carousel.Caption >
+            <h6 className='m-1 text-outline'>Meet {musician.name}</h6>
+            <h6 className='m-1 text-outline'>From {musician.city}</h6>
+            <h6 className='m-1 text-outline'>Plays {musician.instruments.map((instrument, index) => {
+              return index < musician.instruments.length - 1 ? instrument + ', ' : instrument
+            })}</h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        )
+      })}
     </Carousel>
-  );
+  )
+
+
 }
 
 export default Slideshow;
