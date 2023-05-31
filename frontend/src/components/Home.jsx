98fc13a74840bfd77ff261340d6b4c478b1535cc
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-31 07:52:31 +0100

    Add sign up/sign in buttons ; low battery

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
 import Navbar from "./Navbar";
 import Slideshow from "./Slideshow";
 import useFetch from "./useFetch";
 
 
 function Home() {
-    const {data, isPending, error} = useFetch('http://127.0.0.1:7000/api/v1/users/type/musician');
+    const { data, isPending, error } = useFetch('http://127.0.0.1:7000/api/v1/users/type/musician');
 
     return (
         <div className="App">
@@ -17,9 +18,13 @@ function Home() {
                 <h4 className='cinnabar h5'>The best musicians in Abuja at your fingertips</h4>
             </div>
             <div className="carousel-container">
-                {data && <Slideshow data={data}/>}
+                {data && <Slideshow data={data} />}
                 {isPending && <h3>Loading...</h3>}
                 {error && <h4>{error}</h4>}
+            </div>
+            <div className="button-contain m-5 text-center">
+                <Button className='button sign-up'>Sign up</Button>
+                <Button className='button sign-in'>Sign in</Button>
             </div>
         </div>
     )
