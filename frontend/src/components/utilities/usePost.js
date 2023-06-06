c42bf9e3d69d22d59cd04901dc1dba1eb2bc6925
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-06 15:36:32 +0100

    Before trying some change on Post method

--- frontend/src/components/utilities/usePost.js
+++ frontend/src/components/utilities/usePost.js
@@ -3,28 +3,30 @@ import { useState, useEffect } from "react";
 
 function usePost(url, jsonData) {
     const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(null);
 
     fetch(url, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost/3000"
+        },
         body: JSON.stringify(jsonData)
     })
         .then((res) => {
-            if (!res.ok) {
-                throw Error("Failed to create your account");
-            }
             return res.json();
         }).then((data) => {
+            if (data.error && data.error === 'Not Found') {
+                data = ({ "error": "User does not exist" });
+            }
             setData(data);
             setIsPending(false);
-            setError(null);
-        }).catch(error => {
-            setError(error.message);
+        }).catch(err => {
+            console.log(err);
             setIsPending(false);
         })
-    return { data, isPending, error };
+
+    return data;
 }
 
 export default usePost;
