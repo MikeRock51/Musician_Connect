8c3b99ca02ccb0e291ffbe50c109c30ea4ce2e0b
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-08 11:14:29 +0100

    Update user card style

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -4,24 +4,26 @@ function User(props) {
     console.log(user);
     return (
         <div className="d-flex justify-content-center align-items-center">
-            <div className="card mb-3 userCard" style={{ maxWidth: "50%" }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img className="py-auto" src={user.profilePicture} className="img-fluid rounded-start" alt="..." />
+            <div className="card mb-3 text-bg-dark brownShadow userCard" style={{ maxWidth: "55%" }}>
+                <div className="row g-2">
+                    <div className="col-md-4 lightShadow">
+                        <img src={user.profilePicture} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">{`${user.firstName} ${user.lastName}`}</h5>
+                            <h5 className="card-title secondary">{`${user.firstName} ${user.lastName}`}</h5>
                             {user.userType.toLowerCase() === 'musician' &&
-                                <div>
-                                    <h6>Instruments</h6>
-                                    <ul>
+                                <div className="">
+                                    {/* <h6>Instruments</h6> */}
+                                    <div className="list-unstyled row text-nowrap pr-2">
                                         {user.instruments.map((instrument, index) => {
-                                            return <li key={instrument.id}>
-                                                {index < user.instruments.length - 1 ? instrument.name + ", " : instrument.name}
-                                            </li>
+                                            return (
+                                                <h6 className={index === 0 ? "col-sm-6 text-bg-success" : "col-sm-6 text-info"} key={instrument.id}>
+                                                    {`[ ${instrument.name} ]`}
+                                                </h6>
+                                            )
                                         })}
-                                    </ul>
+                                    </div>
                                 </div>}
                             <p className="card-text"></p>
                             <p className="card-text">{user.description}</p>
