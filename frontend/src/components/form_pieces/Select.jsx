b95c372bf5d382d40312eba448337a96fd90723f
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-11 15:08:19 +0100

    Move select error message

--- frontend/src/components/form_pieces/Select.jsx
+++ frontend/src/components/form_pieces/Select.jsx
@@ -2,50 +2,72 @@ import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Select(props) {
-    const [choice, setChoice] = useState('');
-    const navigate = useNavigate();
-    const inputRef = useRef(null);
-    const inputElement = inputRef.current;
-    const [validInput, setValidInput] = useState(inputElement &&
-        choice === '' ? false : true);
+  const [choice, setChoice] = useState("");
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
+  const inputElement = inputRef.current;
+  const [validInput, setValidInput] = useState(
+    inputElement && choice === "" ? false : true
+  );
 
-    function handleChange(event) {
-        const value = event.target.value;
-        setValidInput(value === '' ? false : true);
-        props.onChange(props.name, value, undefined, undefined);
-        setChoice(value);
-        // console.log(validInput);
-    }
+  function handleChange(event) {
+    const value = event.target.value;
+    setValidInput(value === "" ? false : true);
+    props.onChange(props.name, value, undefined, undefined);
+    setChoice(value);
+    // console.log(validInput);
+  }
 
-    return (
-        <div className="">
-            <label className="form-label text-light">{props.text}</label>
-            <select
-                className="form-select"
-                name={props.name}
-                value={props.value ? props.value : choice}
-                ref={inputRef}
-                onChange={handleChange}
-                required>
-                <option value=''>Select...</option>
-                {props.items && props.items.map((item) => {
-                    return <option value={props.name === 'userType' ? item.name :
-                        JSON.stringify(item)} key={item.id}>{item.name}</option>
-                })}
-            </select>
-            {inputElement && !validInput && <p className="pt-2 mb-0 bright">{inputElement.validationMessage}</p>}
-            {props.addButton && <div className="col-sm-12 pt-3">
-                <button
-                    type="submit"
-                    className="btn col-4 anime"
-                    onClick={(event) => {
-                        event.preventDefault();
-                        validInput && navigate("/register");
-                    }}
-                >Confirm</button>
-            </div>}
+  return (
+    <div className="">
+      <label className="form-label text-light">
+        {props.text}
+        {inputElement && !validInput && (
+          <span className="pt-2 mb-0 bright">
+            {" "}
+            [ {inputElement.validationMessage} ]
+          </span>
+        )}
+      </label>
+      <select
+        className="form-select"
+        name={props.name}
+        value={props.value ? props.value : choice}
+        ref={inputRef}
+        onChange={handleChange}
+        required
+      >
+        <option value="">Select...</option>
+        {props.items &&
+          props.items.map((item) => {
+            return (
+              <option
+                value={
+                  props.name === "userType" ? item.name : JSON.stringify(item)
+                }
+                key={item.id}
+              >
+                {item.name}
+              </option>
+            );
+          })}
+      </select>
+      {props.addButton && (
+        <div className="col-sm-12 pt-3">
+          <button
+            type="submit"
+            className="btn col-4 anime"
+            onClick={(event) => {
+              event.preventDefault();
+              validInput && navigate("/register");
+            }}
+          >
+            Confirm
+          </button>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default Select;
