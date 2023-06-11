23df7b8f71df8b22d1e2b87e3cf83efa442119c5
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-11 16:50:13 +0100

    Improve registration verification

--- frontend/src/components/form_pieces/Input.jsx
+++ frontend/src/components/form_pieces/Input.jsx
@@ -1,58 +1,72 @@
 import { useState, useRef } from "react";
 
 function Input(props) {
-    const [userInput, setInput] = useState("");
-    // const [error, setError] = useState(true);
-    const inputRef = useRef(null);
-    const inputElement = inputRef.current;
-    let [validInput, setValidInput] = useState(!inputElement ? true : inputElement.validity.valid);
+  const [userInput, setInput] = useState("");
+  // const [error, setError] = useState(true);
+  const inputRef = useRef(null);
+  const inputElement = inputRef.current;
+  let [validInput, setValidInput] = useState(
+    !inputElement ? true : inputElement.validity.valid
+  );
 
-    function handleChange(event) {
-        inputElement && setValidInput(inputElement.validity.valid);
-        console.log(validInput);
-        const value = event.target.value;
-        setInput(value);
-        // if (inputElement && value.length > 2) {
-        //     setValidInput(inputElement.validity.valid);
-        //     validInput = inputElement.validity.valid;
-        // }
-        props.onChange(props.name, value, undefined, validInput);
-    }
+  function handleChange(event) {
+    inputElement && setValidInput(inputElement.validity.valid);
+    const value = event.target.value;
+    setInput(value);
+    // if (inputElement && value.length > 2) {
+    //     setValidInput(inputElement.validity.valid);
+    //     validInput = inputElement.validity.valid;
+    // }
+    props.onChange(props.name, value, undefined, validInput);
+  }
 
-    return (
-        props.type ?
-            <div className="">
-                <label className="form-label text-light">
-                    {props.text}
-                    {inputElement && !validInput && <span className="pt-2 mb-0 bright"> [ {inputElement.validationMessage} ]</span>}
-                    {props.name === 'confirmPassword' &&
-                        userInput !== props.pwd && userInput.length > 0 &&
-                        <span className="pt-2 mb-0 bright"> [ Password Mismatch ]</span>}
-                </label>
-                <input
-                    type={props.type}
-                    name={props.name}
-                    className="form-control"
-                    placeholder={props.name === 'price_by_hour' ? 'How much do you charge per hour?' : props.text}
-                    required={props.mandatory ? true : false}
-                    value={userInput}
-                    ref={inputRef}
-                    pattern={props.pattern && props.pattern}
-                    onChange={handleChange}
-                    minLength={props.name === 'password' ? "8" : ""}
-                />
-            </div> :
-            <div className="form-floating">
-                <textarea name="description" id={props.id && props.id}
-                    className="form-control"
-                    style={{ height: "100px" }}
-                    onChange={handleChange}
-                    value={userInput}
-                    ref={inputRef}
-                />
-                <label className="form-label" >{props.text}</label>
-            </div>
-    )
+  return props.type ? (
+    <div className="">
+      <label className="form-label text-light">
+        {props.text}
+        {inputElement && !validInput && (
+          <span className="pt-2 mb-0 bright">
+            {" "}
+            [ {inputElement.validationMessage} ]
+          </span>
+        )}
+        {props.name === "confirmPassword" &&
+          userInput !== props.pwd &&
+          userInput.length > 0 && (
+            <span className="pt-2 mb-0 bright"> [ Password Mismatch ]</span>
+          )}
+      </label>
+      <input
+        type={props.type}
+        name={props.name}
+        className="form-control"
+        placeholder={
+          props.name === "price_by_hour"
+            ? "How much do you charge per hour?"
+            : props.text
+        }
+        required={props.mandatory ? true : false}
+        value={userInput}
+        ref={inputRef}
+        pattern={props.pattern && props.pattern}
+        onChange={handleChange}
+        minLength={props.name === "password" ? "8" : ""}
+      />
+    </div>
+  ) : (
+    <div className="form-floating">
+      <textarea
+        name="description"
+        id={props.id && props.id}
+        className="form-control"
+        style={{ height: "100px" }}
+        onChange={handleChange}
+        value={userInput}
+        ref={inputRef}
+      />
+      <label className="form-label">{props.text}</label>
+    </div>
+  );
 }
 
 export default Input;
--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -6,184 +6,276 @@ import useFetch from "../utilities/useFetch";
 import { useNavigate } from "react-router-dom";
 
 function Register(props) {
-    const statesUrl = 'http://127.0.0.1:7000/api/v1/states';
-    const { data: states } = useFetch(statesUrl);
-    const cities = props.userData.state &&
-        JSON.parse(props.userData.state).cities;
-    const navigate = useNavigate();
+  const statesUrl = "http://127.0.0.1:7000/api/v1/states";
+  const { data: states } = useFetch(statesUrl);
+  const cities =
+    props.userData.state && JSON.parse(props.userData.state).cities;
+  const navigate = useNavigate();
 
-    // POST DATAs
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(null);
-    const [error, setError] = useState(null);
-    let [verified, setVerified] = useState(false);
+  // POST DATAs
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(null);
+  let [error, setError] = useState(null);
+  let [verified, setVerified] = useState(false);
 
-    function postIt(url, jsonData) {
-        // POST datas
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "http://localhost/3000"
-            },
-            body: JSON.stringify(jsonData)
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    // console.log(res.json());
-                    // throw Error("Failed to create your account");
-                }
-                return res.json();
-            }).then((data) => {
-                setData(data);
-                setIsPending(false);
-                setError(null);
-            }).catch(err => {
-                setError(err.message);
-                setIsPending(false);
-            })
-    }
+  function postIt(url, jsonData) {
+    // POST datas
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost/3000",
+      },
+      body: JSON.stringify(jsonData),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          // console.log(res.json());
+          // throw Error("Failed to create your account");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }
 
-    function duplicateObject(obj) {
-        return JSON.parse(JSON.stringify(obj));
-    }
+  function duplicateObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
 
-    function handleClicked(event) {
-        event.preventDefault();
-        const requiredFields = ['firstName', 'lastName', 'email',
-            'city_id', 'userType', 'phone', 'password'];
+  function handleClicked(event) {
+    event.preventDefault();
+    const userData = duplicateObject(props.userData);
 
-        requiredFields.map((field) => {
-            // console.log(props.userData[field]);
-            if (!props.userData[field] || props.userData[field].length < 2) {
-                setVerified(false);
-            }
+    // Parse city & instrument data
+    for (let key in userData) {
+      if (key === "city" || key === "state") {
+        const id = key + "_id";
+        userData[id] = JSON.parse(userData[key]).id;
+        delete userData[key];
+      }
+      if (key === "instruments") {
+        const instrumentIds = userData[key].map((instrument) => {
+          return instrument.id;
         });
+        userData[key] = instrumentIds;
+      }
+    }
+    handleSubmit(userData);
+    // props.isValid && handleSubmit();
+    // !props.isValid && alert(
+    //     'One or more invalid responses, please check and try again...')
+  }
 
-        // props.isValid && handleSubmit();
-        // !props.isValid && alert(
-        //     'One or more invalid responses, please check and try again...')
+  function handleSubmit(userData) {
+    const userPostUrl = "http://127.0.0.1:7000/api/v1/users";
+    const requiredFields = [
+      "firstName",
+      "lastName",
+      "email",
+      "city_id",
+      "userType",
+      "phone",
+      "password",
+    ];
+
+    //   Verify that password and confirm password fields match
+    if (userData.password !== userData.confirmPassword) {
+      setError(true);
+      error = true;
+      setVerified(false);
+      verified = false;
+      return;
+    } else {
+      setError(null);
+      error = null;
+      setVerified(true);
+      verified = true;
+      console.log("Passed");
     }
 
-    function handleSubmit() {
-        const userPostUrl = 'http://127.0.0.1:7000/api/v1/users';
-        const user = duplicateObject(props.userData);
+    //   console.log(userData);
 
-        // Verify that password and confirm password fields match
-        if (props.userData.password !== props.userData.confirmPassword) {
-            alert('Password Mismatch');
-            setVerified(false);
-        } else {
-            setVerified(true);
-            verified = true;
-        }
-
-        // Verify that all required fields have value
-        for (let key in user) {
-            if (user[key] !== 'profilePicture' &&
-                user[key].length < 2) {
-                alert(`${key} cannot be empty`)
-                setVerified(false);
-            } else {
-                setVerified(true);
-                verified = true;
-            }
-
-            if (key === 'city' || key === 'state') {
-                const id = key + '_id';
-                user[id] = JSON.parse(user[key]).id;
-                delete user[key];
-            }
-
-            if (key === 'instruments') {
-                const instrumentIds = user[key].map((instrument) => {
-                    return instrument.id;
-                });
-                user[key] = instrumentIds;
-            }
-        }
-
-        if (verified) {
-            console.log(user);
-            postIt(userPostUrl, user);
-            console.log(data);
-            !isPending && !error && navigate('/sign-in');
-            error && console.log(error);
-        } else { alert('Please fill all required fields'); }
+    // Verify that all required fields are present and not empty
+    for (let field of requiredFields) {
+      if (!userData[field] || userData[field].length < 1) {
+        console.log(field + ": " + userData[field]);
+        setVerified(false);
+        verified = false;
+        setError(true);
+        error = true;
+        return;
+      } else {
+        setError(null);
+        error = null;
+        setVerified(true);
+        verified = true;
+      }
     }
 
-    return (
-        <div className="container-md">
-            <div className="alert alert-danger alert-dismissible fade show text-center" role="alert">
-                <strong className="">Input error! Please check the form and try again.</strong>
-                <button type="button" className="btn-close pe-3" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-            <form className="row g-3 mx-5">
-                <div className={props.userData.userType === 'Client' ? 'col-md-12' : "col-md-6"}>
-                    <Select name="userType"
-                        items={[
-                            { 'name': 'Client', 'id': 1 },
-                            { 'name': 'Musician', 'id': 2 }
-                        ]}
-                        text="Account Type"
-                        onChange={props.onChange}
-                        value={props.userData.userType}
-                    />
-                </div>
-                {props.userData.userType === 'Musician' && <Checklist
-                    onChange={props.onChange}
-                    name='instruments'
-                    checkedItems={props.userData.instruments}
-                    lenCheckedItems={props.userData.instruments && props.userData.instruments.length}
-                />}
-                <div className="col-md-6">
-                    <Input type="text" name="firstName" text="First Name"
-                        mandatory={true} onChange={props.onChange} />
-                </div>
-                <div className="col-md-6">
-                    <Input type="text" name="lastName" text="Last Name"
-                        mandatory={true} onChange={props.onChange} />
-                </div>
-                <div className="col-md-6">
-                    <Input type="email" name="email" text="Email"
-                        mandatory={true} onChange={props.onChange} />
-                </div>
-                <div className="col-md-6">
-                    <Input type="tel" name="phone" text="Phone Number"
-                        mandatory={true} onChange={props.onChange}
-                        pattern="^\+?[0-9\s-]+$" />
-                </div>
-                <div className="col-md-6">
-                    <Input type="password" name="password" text="Password"
-                        mandatory={true} onChange={props.onChange} />
-                </div>
-                <div className="col-md-6">
-                    <Input type="password" name="confirmPassword"
-                        text="Confirm Password" pwd={props.userData.password}
-                        mandatory={true} onChange={props.onChange} />
-                </div>
-                <div className="col-md-6">
-                    <Select name="state" items={states} text="State"
-                        onChange={props.onChange} />
-                </div>
-                <div className="col-md-6">
-                    <Select name="city"
-                        items={cities}
-                        onChange={props.onChange} text="City" />
-                </div>
-                {props.userData.userType === 'Musician' && <Input
-                    type="text" name="alias" text="Alias"
-                    mandatory={false} onChange={props.onChange} />}
-                {props.userData.userType === 'Musician' && <Input type="text" name="price_by_hour" text="Price Per Hour"
-                    mandatory={true} onChange={props.onChange} />}
+    if (verified) {
+        console.log("Success");
+    //   console.log(user);
+    //   postIt(userPostUrl, userData);
+    //   console.log(data);
+    //   !isPending && !error && navigate("/sign-in");
+    //   error && console.log(error);
+    } else {
+      alert("Please fill all required fields");
+    }
+  }
 
-                <Input name="description"
-                    id={props.userData.id && props.userData.id}
-                    text="Write a brief description about yourself:"
-                    onChange={props.onChange}
-                />
+  return (
+    <div className="container-md">
+      {error && !verified && (
+        <div
+          className="alert alert-danger alert-dismissible fade show text-center fixed-top mt-5"
+          role="alert"
+        >
+          <strong className="">
+            One or more invalid responses, please check and try again...
+          </strong>
+          <button
+            type="button"
+            className="btn-close pe-3"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      )}
+      <form className="row g-3 mx-5">
+        <div
+          className={
+            props.userData.userType === "Client" ? "col-md-12" : "col-md-6"
+          }
+        >
+          <Select
+            name="userType"
+            items={[
+              { name: "Client", id: 1 },
+              { name: "Musician", id: 2 },
+            ]}
+            text="Account Type"
+            onChange={props.onChange}
+            value={props.userData.userType}
+          />
+        </div>
+        {props.userData.userType === "Musician" && (
+          <Checklist
+            onChange={props.onChange}
+            name="instruments"
+            checkedItems={props.userData.instruments}
+            lenCheckedItems={
+              props.userData.instruments && props.userData.instruments.length
+            }
+          />
+        )}
+        <div className="col-md-6">
+          <Input
+            type="text"
+            name="firstName"
+            text="First Name"
+            mandatory={true}
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <Input
+            type="text"
+            name="lastName"
+            text="Last Name"
+            mandatory={true}
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <Input
+            type="email"
+            name="email"
+            text="Email"
+            mandatory={true}
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <Input
+            type="tel"
+            name="phone"
+            text="Phone Number"
+            mandatory={true}
+            onChange={props.onChange}
+            pattern="^\+?[0-9\s-]+$"
+          />
+        </div>
+        <div className="col-md-6">
+          <Input
+            type="password"
+            name="password"
+            text="Password"
+            mandatory={true}
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <Input
+            type="password"
+            name="confirmPassword"
+            text="Confirm Password"
+            pwd={props.userData.password}
+            mandatory={true}
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <Select
+            name="state"
+            items={states}
+            text="State"
+            onChange={props.onChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <Select
+            name="city"
+            items={cities}
+            onChange={props.onChange}
+            text="City"
+          />
+        </div>
+        {props.userData.userType === "Musician" && (
+          <Input
+            type="text"
+            name="alias"
+            text="Alias"
+            mandatory={false}
+            onChange={props.onChange}
+          />
+        )}
+        {props.userData.userType === "Musician" && (
+          <Input
+            type="text"
+            name="price_by_hour"
+            text="Price Per Hour"
+            mandatory={true}
+            onChange={props.onChange}
+          />
+        )}
 
-                {/* <div className="mb-3 col-lg-6">
+        <Input
+          name="description"
+          id={props.userData.id && props.userData.id}
+          text="Write a brief description about yourself:"
+          onChange={props.onChange}
+        />
+
+        {/* <div className="mb-3 col-lg-6">
                 <label className="form-label">Upload profile picture:</label>
                 <input type="file" className="form-control form-control"
                     id="formFileSm" name="profilePicture" accept="image/*"
@@ -193,23 +285,29 @@ function Register(props) {
                     enctype="multipart/form-data"
                 />
             </div> */}
-                <div className="col-12">
-                    <button type="submit" className="btn anime w-100 mb-3"
-                        onClick={handleClicked}
-                    >
-                        {!isPending && !error && "Create my account"}
-                        {isPending && "Creating your account..."}
-                        {error && error}
-                    </button>
-                    <span className="text-light">
-                        Already have an account?
-                        <a href="/sign-in"
-                            className="link-underline link-underline-opacity-0 hover bright"> Sign In here</a>
-                    </span>
-                </div>
-            </form>
+        <div className="col-12">
+          <button
+            type="submit"
+            className="btn anime w-100 mb-3"
+            onClick={handleClicked}
+          >
+            {!isPending && "Create my account"}
+            {isPending && "Creating your account..."}
+          </button>
+          <span className="text-light">
+            Already have an account?
+            <a
+              href="/sign-in"
+              className="link-underline link-underline-opacity-0 hover bright"
+            >
+              {" "}
+              Sign In here
+            </a>
+          </span>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
 export default Register;
