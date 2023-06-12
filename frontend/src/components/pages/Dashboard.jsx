019ccabb0fe6de8c3e17b8d29a8a88b593393a4f
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-12 11:28:29 +0100

    Update Dashboard UI

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -7,8 +7,8 @@ function Dashboard(props) {
   let [respondentId, setRespondentId] = useState(null);
 
   return (
-    <div className="container-sm">
-      <div className="row px-2">
+    <div className="container-sm container-fluid">
+      <div className="row px-1">
         <h3 className="ps-0 pinky shadow-text-light">Welcome {user && user.firstName}</h3>
         <h6
           className="col-sm-3 col-3 bg-brownie text-center p-1 my-2 rounded bright"
