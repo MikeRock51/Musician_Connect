5ad33d0a39164a1ce6ad851912f2c777bebecbdb
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-16 06:02:30 +0100

    Add landing page about section

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -5,9 +5,7 @@ import useFetch from "../utilities/useFetch";
 import Quote from "../Quote";
 
 function Home(props) {
-  const { data, isPending, error } = useFetch(
-    "/users/type/musician"
-  );
+  const { data, isPending, error } = useFetch("/users/type/musician");
   const navigate = useNavigate();
 
   return (
@@ -16,7 +14,7 @@ function Home(props) {
         <h1 className="display-5 fw-semibold bright">
           Welcome to Musician Connect!
         </h1>
-        <h4 className="fs-5 px-4 fw-semibold display-6 text-info h5">
+        <h4 className="fs-5 px-4 fw-semibold display-6 text-light h5">
           The best musicians in Abuja at your fingertips
         </h4>
       </div>
@@ -49,7 +47,9 @@ function Home(props) {
       </div>
 
       <div className="col-md-6 col-sm-9">
-        <h4 className="text-info text-center display-5 fw-semibold mt-5 mb-3">Featured Users</h4>
+        <h4 className="text-info text-center display-5 fw-semibold mt-5 mb-3">
+          Featured Users
+        </h4>
         <div className="carousel-container bg-secondary p-0 mt-3 w-75">
           {data && <Slideshow data={data} />}
           {isPending && <h3>Loading...</h3>}
@@ -74,7 +74,22 @@ function Home(props) {
           </button>
         </div>
       </div>
-      {/* } */}
+      <div className="text-center mt-4 px-5 py-3 bg-light col-10">
+        <h4 className="brownie fw-semibold">About Musician Connect</h4>
+        <p className="text-dark text-start">
+          Musician Connect is an online platform to help people who are in need
+          of skilled musicians connect with them. Musicians get to register for
+          a public profile on the platform, where their skill set is on display
+          for potential clients to see. Likewise, it will help clients who are
+          in need of musical services easily find what they are looking for, as
+          well as connect fellow musicians for social and developmental events.
+          As a musician myself, I thought a platform like this would be valuable
+          to both musicians and clients. Because it provides a platform to
+          showcase the musicians, while also making it easier for clients to
+          access the unique services and talent of every musician around their
+          city.
+        </p>
+      </div>
     </div>
   );
 }
