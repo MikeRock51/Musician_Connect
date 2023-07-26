dbd8c69e848efb39444ac31429a0d3edd3c76224
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-26 10:05:10 +0100

    Comment User model/Fix pycodestyle errors

--- models/user.py
+++ models/user.py
@@ -9,19 +9,21 @@ from sqlalchemy.orm import relationship
 
 defaultImage = "https://cdn-icons-png.flaticon.com/512/4123/4123763.png"
 
-# Many to many relationship between a musician user and instrument
+# Association table to represent a many to many relationship between
+# a musician user and the instruments they play
 musicianInstruments = Table('musicianInstruments', Base.metadata,
                             Column('user_id', String(60), ForeignKey(
                                 'users.id'), nullable=False, primary_key=True),
                             Column('instrument_id', String(60), ForeignKey(
-                                'instruments.id'), nullable=False, primary_key=True), extend_existing=True)
+                                'instruments.id'), nullable=False,
+                                primary_key=True), extend_existing=True)
 
 
 class User(BaseModel, Base):
     """The User model"""
     __tablename__ = "users"
 
-    userType = Column(String(20), nullable=False)
+    userType = Column(String(20), nullable=False)  # Client or Musician
     firstName = Column(String(128), nullable=False)
     lastName = Column(String(128), nullable=False)
     email = Column(String(128), nullable=False, unique=True)
@@ -32,14 +34,24 @@ class User(BaseModel, Base):
     description = Column(String(1024), nullable=True)
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     rating = Column(Integer, nullable=False, default=5)
+    # Stores a list of reviews by a client
     clientReviews = relationship(
-        'Review', backref='client', cascade='all, delete', foreign_keys=[Review.client_id])
+        'Review', backref='client', cascade='all, delete',
+        foreign_keys=[Review.client_id])
+    # Stores a list of reviews by a musician
     musicianReviews = relationship(
-        'Review', backref='musician', cascade='all, delete', foreign_keys=[Review.musician_id])
+        'Review', backref='musician', cascade='all, delete',
+        foreign_keys=[Review.musician_id])
+    # Stores a list of bookings by a client
     clientBookings = relationship('Booking', foreign_keys=[Booking.client_id])
+    # Stores a list of bookings by a musician
     musicianBookings = relationship(
         'Booking', foreign_keys=[Booking.musician_id])
     alias = Column(String(128), nullable=True)
+    # Stores a list of instruments played by a musician
+    # and sets a reverse list of players in the instruments table
     instruments = relationship('Instrument',
-                               secondary='musicianInstruments', backref='players', cascade='all, delete', viewonly=False)
+                               secondary='musicianInstruments',
+                               backref='players', cascade='all, delete',
+                               viewonly=False)
     price_by_hour = Column(Integer, nullable=False, default=0)
